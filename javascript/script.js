b3c1f8ef01a4bfc57e1268adc98e64efb9e753f2
Felipe Guimarães <felipe700guima@gmail.com>
2025-05-07 21:48:12 -0300

    ajustando bug no menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,22 @@
 function openNav(){
-    document.querySelector(".links-nav-mobile").style.width = "50%";
     document.querySelector(".abrir").style.display = "none";
     document.querySelector(".fechar").style.display = "block";
+    document.querySelector(".links-nav-mobile").style.width = "50%";
 }
 
 function closeNav(){
-    document.querySelector(".links-nav-mobile").style.width = "0%";
     document.querySelector(".abrir").style.display = "block";
     document.querySelector(".fechar").style.display = "none";
+    document.querySelector(".links-nav-mobile").style.width = "0%";
 }
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+        // Fecha o menu mobile e reseta ícones ao redimensionar
+        document.querySelector(".links-nav-mobile").style.width = "0%";
+        document.querySelector(".abrir").style.display = "none";
+        document.querySelector(".fechar").style.display = "none";
+    } else {
+        document.querySelector(".abrir").style.display = "block";
+    }
+});
